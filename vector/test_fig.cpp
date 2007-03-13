039d1c9aa616a8bcd99243a91d165a3e07db402e
slazav <slazav>
2007-03-13 10:32:32 +0000

    *** empty log message ***

--- vector/test_fig.cpp
+++ vector/test_fig.cpp
@@ -7,10 +7,11 @@ main(int argc, char **argv){
     exit(0);
   }
   fig::fig_world W = fig::read(argv[1]);
-  fig::fig_world W1;
-
   std::cerr << W.size() << " objects\n";
 
+  fig::fig_world W1;
+
+  // выбрать все объекты такого вида:
   for (fig::fig_world::const_iterator i=W.begin(); i!=W.end(); i++){
     if (test_object(*i, "3 * * * 26 * 90 * * * * * * *"))
       W1.push_back(*i);
