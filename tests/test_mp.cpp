4d4b7246f0ad1e9fc2cc6689374cf728f03b6227
Vladislav Zavjalov <slazav@altlinux.org>
2008-10-18 13:33:20 +0400

    add tests/test_mp.cpp

---
+++ tests/test_mp.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <fstream>
+#include "../libmp/mp.h"
+
+using namespace std;
+
+int main(int argc, char** argv){
+    if (argc < 3){
+      cerr << "usage: test_mp <in> <out>\n";
+      exit(1);
+    }
+
+    mp::mp_world M;
+    if (!mp::read(argv[1], M)) {
+      cerr << "ERR: bad mp file\n"; exit(1);
+    }
+
+    ofstream out(argv[2]);
+    mp::write(out, M);
+}
