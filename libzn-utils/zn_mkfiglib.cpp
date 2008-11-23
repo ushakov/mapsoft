b79a81d815f2643dda9ff81959d92caaca1be5f7
Vladislav Zavjalov <slazav@altlinux.org>
2008-11-23 15:31:11 +0300

    add libzn-utils/zn_mkfiglib for making xfig libs

---
+++ libzn-utils/zn_mkfiglib.cpp
@@ -0,0 +1,34 @@
+// Получение fig-библиотеки со всеми знаками
+
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <iomanip>
+#include "../libzn/zn_lists.h"
+
+using namespace std;
+
+void usage(){
+    cerr << "usage: zn_mkfiglib <conf_file> <out_dir>\n";
+    exit(0);
+}
+
+main(int argc, char** argv){
+
+  if (argc != 3) usage();
+  string conf_file = argv[1];
+  string out_dir = argv[2];
+
+  zn::zn_conv z(conf_file);
+  for (map<int, zn::zn>::const_iterator i = z.znaki.begin(); i!=z.znaki.end(); i++){
+    fig::fig_object o=make_sample(i);
+    fig::fig_world F;
+    list<fig::fig_object> l=z.make_pic(o, i->first);
+    F.push_back(o);
+    F.insert(F.end(), l.begin(), l.end());
+    ostringstream name;
+    name << out_dir << "/0x" << setw(6) << setfill('0') << setbase(16) << i->first << setbase(10) << ".fig";
+    ofstream out(name.str().c_str());
+    fig::write(out, F);
+  }
+}
