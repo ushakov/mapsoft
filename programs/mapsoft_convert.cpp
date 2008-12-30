a575193da49394f759fb88eddc56ca67b557ca57
Vladislav Zavjalov <slazav@altlinux.org>
2008-12-30 16:07:16 +0300

    mapsoft_convert.cpp: fix help message

--- programs/mapsoft_convert.cpp
+++ programs/mapsoft_convert.cpp
@@ -16,23 +16,42 @@
 using namespace std;
 
 void usage(const char *fname){
-  cerr << "Usage: "<< fname << " <in1> ... <inN> -O option1[=value1] ... -o <out>\n";
 
-cerr <<
-""<<
-"  -O skip=<string>\n" <<
-"  -s <string>\n" <<
-"                    -- Skipping data\n" <<
-"\n" <<
-"  If <string> contains letter:\n" <<
-"    \"m\" - skip maps\n" <<
-"    \"w\" - skip waypoints\n" <<
-"    \"t\" - skip tracks\n" <<
-"    \"a\" - skip ACTIVE LOG track\n" <<
-"    \"o\" - skip all but ACTIVE LOG tracks\n" <<
-"\n";
+  cerr << "\nConvert geodata to different formats\n"
+       << "Usage: "<< fname << " <in1> ... <inN> --option[=value] ... -o <out>\n\n";
 
-// не все так просто, надо будет написать подробнее...
+  cerr << "Options:\n"
+       << "  --help, -h                    Show this help message\n"
+       << "  --out=<file>, -o <file>       Specify output file\n"
+       << "  --skip=<string>, -s <string>  Skip some types of data\n"
+       << "  --rescale_maps <factor>       Rescale map references\n"
+       << "\n";
+
+  cerr << "Output file. You must provide name for output file (-o, --out option).\n"
+       << "File format is determined by its extension:\n"
+       << "  .xml -- mapsoft XML-like format (all data will be written to one file)\n"
+       << "  .gu  -- old garmin-utils format (tracks and waypoints only)\n"
+       << "  .wpt, .plt, .map, .oe  -- OziExplorer format.\n"
+       << "      (Each track in single .plt file, each waypoint set in\n"
+       << "       single .wpt file, each map reference in one .map file)\n"
+       << "  .zip -- zipped OziExplorer format\n"
+       << "  .kml -- ???\n"
+       << "  .bmp,.png,.jpg -- ???\n"
+       << "  .fig           -- ???\n"
+       << "  .htm,.html     -- ???\n"
+   << "If output file is \"usb:\" data will be sent to Garmin GPS\n"
+       << "device via libusb. If output file is a character device\n"
+       << "data will be sent to Garmin GPS via serial port.\n"
+       << "\n";
+
+  cerr << "Skipping data. If --skip parameter string contains letter:\n"
+       << "  \"m\" -- skip maps\n"
+       << "  \"w\" -- skip waypoints\n"
+       << "  \"t\" -- skip tracks\n"
+       << "  \"a\" -- skip ACTIVE LOG track\n"
+       << "  \"o\" -- skip all but ACTIVE LOG tracks\n"
+       << "\n";
+
   exit(0);
 }
 
