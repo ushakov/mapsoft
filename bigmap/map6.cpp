5777717869e8a486d625f18a1077e13c9ae1c64e
Vladislav Zavjalov <slazav@altlinux.org>
2009-02-22 09:47:34 +0000

    fix bigmap library and examples

--- bigmap/map6.cpp
+++ bigmap/map6.cpp
@@ -1,4 +1,4 @@
-// Склейка четырех карт
+// Склейка шести карт
 // параметры читаются с stdout.
 // (см. файл params)
 
@@ -10,25 +10,25 @@
 
 main(int argc, char **argv){
 
-  int X1[4], X2[4], X3[4], X4[4];
-  int Y1[4], Y2[4], Y3[4], Y4[4];
-  char N[4][30];
+  int X1[6], X2[6], X3[6], X4[6];
+  int Y1[6], Y2[6], Y3[6], Y4[6];
+  char N[6][30];
   int i;
 
 
-  for (i=0; i<4; i++){
+  for (i=0; i<6; i++){
     scanf("%s %d %d %d %d %d %d %d %d\n", &N[i],
     &X1[i], &Y1[i], &X2[i], &Y2[i], &X3[i], &Y3[i], &X4[i], &Y4[i]);
-    printf("%s -- %d %d %d %d %d %d %d %d\n", N[i], 
+    printf("%s -- %d %d %d %d %d %d %d %d\n", N[i],
     X1[i], Y1[i], X2[i], Y2[i], X3[i], Y3[i], X4[i], Y4[i]);
   }
 
-  bm_map M(2,2);
+  bm_map M(3,2);
 
-  for (i=0; i<4; i++){
-    M.add(image_jpeg::load(N[i]), i%2+1, i/2+1,
+  for (i=0; i<6; i++){
+    M.add(image_jpeg::load(N[i]), i%3+1, i/3+1,
       X1[i], Y1[i], X2[i], Y2[i], X3[i], Y3[i], X4[i], Y4[i]);
-    if (i%2==0) M.vert(1, i/2+1, X2[i],Y2[i], X4[i],Y4[i]);
+//    if (i%3==0) M.vert(1, i/3+1, X2[i],Y2[i], X4[i],Y4[i]);
   }
   image_jpeg::save(M.make(), "out.jpg", 95);
 
