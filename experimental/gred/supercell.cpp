94e079447c2fd19cbbb0ac0a73c590bdaa282f9d
Max Ushakov <ushakov@frog.(none)>
2009-06-04 21:32:45 +0400

    add supercell class and test

---
+++ experimental/gred/supercell.cpp
@@ -0,0 +1,44 @@
+#include "supercell.h"
+
+const uint64_t SuperCell::ROOT = (1ULL << 63);
+
+SuperCell SuperCell::from_point(Point<GCoord> p, int level) {
+  uint64_t one = ROOT;
+  uint64_t id = 0;
+  GCoord tlx = GCoord_min;
+  GCoord tly = GCoord_min;
+  GCoord size = GCoord_max - GCoord_min;
+  int cur_level = 0;
+  while (cur_level < level) {
+    size /= 2;
+    if (p.x >= tlx + size) {
+      id |= one;
+      tlx += size;
+    }
+    one >>= 1;
+    if (p.y >= tly + size) {
+      id |= one;
+      tly += size;
+    }
+    one >>= 1;
+    cur_level++;
+  }
+  // Mark the end
+  id |= one;
+  return SuperCell(id);
+}
+
+SuperCell SuperCell::LCA(SuperCell a, SuperCell b) {
+  uint64_t mask = ROOT | (ROOT >> 1);
+  while ((a.id & mask) == (b.id & mask)) {
+    mask >>= 2;
+  }
+  // mask is 11000...
+  // we need to clean a.id at those positions and add 10000...
+  mask = (mask - 1) ^ mask;  // 01111...
+  uint64_t one = mask + 1;  // 10000...
+  mask <<= 1; mask += 1;  // 11111...
+  uint64_t new_id = a.id & (~mask);
+  new_id |= one;
+  return SuperCell(new_id);
+}
