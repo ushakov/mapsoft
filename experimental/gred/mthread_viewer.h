7eaf47d0323e2dd7a07c3ee567fb3ff3bdcd5620
Vladislav Zavjalov <slazav@altlinux.org>
2009-06-23 16:31:07 +0400

    move code from mthread_viewer.h to mthread_viewer.cpp

--- experimental/gred/mthread_viewer.h
+++ experimental/gred/mthread_viewer.h
@@ -1,57 +1,18 @@
-#include <gtkmm.h>
-
-#include <queue>
-#include <map>
+#ifndef MTHREAD_VIEWER
+#define MTHREAD_VIEWER
 
 #include "simple_viewer.h"
+#include <map>
 
 class MThreadViewer : public SimpleViewer {
   public:
 
-    MThreadViewer(GPlane * pl1, GPlane * pl2) :
-        SimpleViewer(pl1),
-        slow_plane(pl2){
+    MThreadViewer(GPlane * pl1, GPlane * pl2);
+    ~MThreadViewer();
 
-      Glib::thread_init();
-      done_signal.connect(sigc::mem_fun(*this, &MThreadViewer::on_done_signal));
-
-      mutex = new(Glib::Mutex);
-    }
-    ~MThreadViewer(){
-      delete(mutex);
-    }
-
-  void updater(const Rect<int> & r){
-    std::pair<Point<int>, Image<int> > p(r.TLC(), slow_plane->draw(r));
-
-    mutex->lock();
-    done_cache.insert(p);
-    mutex->unlock();
-
-    done_signal.emit();
-  }
-
-  void on_done_signal(){
-    mutex->lock();
-    for (std::map<Point<int>,Image<int> >::const_iterator
-         i=done_cache.begin(); i!=done_cache.end(); i++){
-
-      draw_image(i->second, i->first-origin);
-    }
-    done_cache.clear();
-    mutex->unlock();
-  }
-
-  void draw(const Rect<int> & r){
-    if (r.empty()) return;
-    draw_image(plane->draw(r + origin), r.TLC());
-
-    Glib::Thread * tile_updater_thread =
-      Glib::Thread::create(
-        sigc::bind<1>(sigc::mem_fun(*this, &MThreadViewer::updater),
-          r + origin),
-        false);
-  }
+    void updater(const Rect<int> & r);
+    void on_done_signal();
+    void draw(const Rect<int> & r);
 
   private:
     GPlane *slow_plane;
@@ -61,3 +22,4 @@ class MThreadViewer : public SimpleViewer {
     Glib::Mutex              *mutex;
 };
 
+#endif
