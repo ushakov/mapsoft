524865675acacfb4f5e15e6599538cef8afe92f8
Vladislav Zavjalov <slazav@altlinux.org>
2009-07-01 03:18:10 +0400

    gred/action_viewer_test1.cpp: add new test action to draw polylines; use control key to erase points

--- experimental/gred/action_viewer_test1.cpp
+++ experimental/gred/action_viewer_test1.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include "../../core/lib2d/line.h"
 #include "gplane.h"
 #include "action_viewer.h"
 #include "dthread_viewer.h"
@@ -6,11 +7,12 @@
 
 typedef ActionViewer<RubberViewer<DThreadViewer> > ViewerT;
 
-class ActionTest : public Action{
+class ActionTest1 : public Action{
   public:
-  ActionTest(ViewerT * v) : viewer(v) { }
+  ActionTest1(ViewerT * v) : viewer(v) { }
 
   std::string get_name() { return "Test1"; }
+
   virtual void init() {
     clear=true;
   }
@@ -33,6 +35,39 @@ class ActionTest : public Action{
   bool clear;
 };
 
+class ActionTest2 : public Action{
+  public:
+  ActionTest2(ViewerT * v) : viewer(v) { }
+
+  std::string get_name() { return "Test2"; }
+
+  virtual void init() {
+    viewer->rubber_clear();
+    data.clear();
+  }
+  virtual void reset() {
+    viewer->rubber_clear();
+    data.clear();
+  }
+  virtual void click(iPoint p) {
+    int x,y;
+    Gdk::ModifierType state;
+    viewer->rubber_clear();
+
+    viewer->get_window()->get_pointer(x,y,state);
+    if (state && Gdk::CONTROL_MASK) data.resize(data.size()-1);
+    else data.push_back(p);
+
+    viewer->rubber_add_diag(data[data.size()-1]);
+    for (int i=0; i<data.size()-1; i++){
+      viewer->rubber_add(data[i], 0, data[i+1], 0);
+    }
+  }
+  ViewerT * viewer;
+  iLine data;
+  bool clear;
+};
+
 int main(int argc, char **argv){
 
     Gtk::Main     kit (argc, argv);
@@ -40,9 +75,11 @@ int main(int argc, char **argv){
     GPlaneTestGridSlow p1;
 
     ViewerT viewer(&p1);
-    ActionTest A1(&viewer);
+    ActionTest1 A1(&viewer);
+    ActionTest2 A2(&viewer);
     viewer.action_add(&A1);
-    viewer.action_select(0);
+    viewer.action_add(&A2);
+    viewer.action_select(1);
 
     win.add(viewer);
     win.set_default_size(640,480);
