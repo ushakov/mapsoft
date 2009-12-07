a9438d7d74cc050a320f05e25ad4416f3711bc0a
Vladislav Zavjalov <slazav@altlinux.org>
2009-12-07 20:30:29 +0300

    viewer: fix generic_dialog (in progress)

--- viewer/generic_dialog.cpp
+++ viewer/generic_dialog.cpp
@@ -1,30 +1,48 @@
 #include "generic_dialog.h"
 
+/*
+There are four ways to clear dialog
+ - dialog button is pressed
+ - dialog window is closed
+ - deactivate()
+ - activate()
+
+Before clearing on_res callback must be called.
+, but only once.
+
+*/
+
 GenericDialog::GenericDialog (){
 
-    add_button (Gtk::Stock::OK, 0);
-    add_button (Gtk::Stock::CANCEL, 1);
+    add_button (Gtk::Stock::OK,     Gtk::RESPONSE_OK);
+    add_button (Gtk::Stock::CANCEL, Gtk::RESPONSE_CANCEL);
+    // when dialog is closed Gtk::RESPONSE_NONE response emitted
 
     signal_response().connect(
       sigc::mem_fun (this, &GenericDialog::on_response));
-    signal_delete_event().connect(
-      sigc::mem_fun (this, &GenericDialog::on_delete));
+
     table=NULL;
 }
-
 
 void GenericDialog::activate (
       const std::string & title,
       const Options & options,
       const sigc::slot2<void,int,Options> & _on_res){
 
-  deactivate();
+  deactivate(); // deactivate old dialog
+
+  if (table){ // we can't do it in on_response callback
+    get_vbox()->remove(*table);
+    delete table;
+  }
+
   on_res = _on_res;
   set_title(title);
 
   table = new Gtk::Table;
   table->resize(options.size(), 2);
 
+  entries.clear();
   int k = 0;
   for (Options::const_iterator i = options.begin(); i != options.end(); ++i, ++k) {
     Gtk::Label * label = Gtk::manage(new Gtk::Label (i->first, 1.0, 0.5));
@@ -37,35 +55,35 @@ void GenericDialog::activate (
     entries[i->first]=entry;
   }
   get_vbox()->add(*table);
+
+  resize(1,1);
+  deiconify();
   show_all();
+  show_now();
+std::cerr << ">>> SHOW\n";
 }
 
+// deactivate is a special case of on_response
+void GenericDialog::deactivate () {
+  on_response(Gtk::RESPONSE_CANCEL);
+}
+
+// run on_res callback, deactivates it, hide dialog
 void GenericDialog::on_response (int response) {
-  Options options;
-  if (response==0){
-    for (std::map<std::string, Gtk::Entry *>::const_iterator i=entries.begin(); i!=entries.end();i++){
+
+  if (response==Gtk::RESPONSE_OK){
+    Options options;
+    for (std::map<std::string, Gtk::Entry *>::const_iterator
+          i=entries.begin(); i!=entries.end();i++){
       options[i->first]  = i->second->get_text();
     }
+    on_res(0, options);
   }
-  on_res(response, options);
-  clear();
-}
-
-bool GenericDialog::on_delete (GdkEventAny * e) {
-  on_res(0, Options());
-  clear();
-}
-
-void GenericDialog::deactivate () {
-  on_res(0, Options());
-  clear();
-}
-
-void GenericDialog::clear () {
-  entries.clear();
+  else {
+    on_res(1, Options());
+  }
+  on_res=sigc::slot2<void,int,Options>(); // we don't want to call it any more
   hide_all();
-  if (table!=NULL) {
-    get_vbox()->remove(*table);
-//    delete table;
-  }
+std::cerr << ">>> HIDE\n";
 }
+
