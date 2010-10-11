18166f02d9d9488b68632ba1b775f223ce68f4e4
Vladislav Zavjalov <slazav@altlinux.org>
2010-10-12 00:58:11 +0400

    vector/data: move some programs from libzn-utils; fix makefile

--- vector/data/zn_figlib.cpp
+++ vector/data/zn_figlib.cpp
@@ -4,19 +4,25 @@
 #include <sstream>
 #include <iomanip>
 #include "vmap/zn_lists.h"
+#include <sys/stat.h>
 
 using namespace std;
 
 void usage(){
     cerr << "usage: zn_mkfiglib <style> <out_dir>\n";
-    exit(0);
+    exit(1);
 }
 
 main(int argc, char** argv){
 
   if (argc != 3) usage();
-  string style   = argv[1];
-  string out_dir = argv[2];
+  const char * style   = argv[1];
+  const char * out_dir = argv[2];
+
+  if (mkdir(out_dir, 0755) != 0){
+    cerr << "can't create directory: " << out_dir << "\n";
+    exit(1);
+  }
 
   zn::zn_conv z(style);
   for (map<int, zn::zn>::const_iterator i = z.znaki.begin(); i!=z.znaki.end(); i++){
@@ -24,7 +30,8 @@ main(int argc, char** argv){
     list<fig::fig_object> l=z.make_pic(make_sample(i), i->first);
     F.insert(F.end(), l.begin(), l.end());
     ostringstream name;
-    name << out_dir << "/0x" << setw(6) << setfill('0') << setbase(16) << i->first << setbase(10) << ".fig";
+    name << out_dir << "/0x" << setw(6) << setfill('0')
+         << setbase(16) << i->first << ".fig";
     fig::write(name.str(), F);
   }
 }
