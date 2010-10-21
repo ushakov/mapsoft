1a730aee8726cb14f06525c3ad54940ebd964791
Vladislav Zavjalov <slazav@altlinux.org>
2010-10-21 08:05:02 +0400

    libvmap: fix border calculation accuracy

--- core/vmap/vmap_filt.cpp
+++ core/vmap/vmap_filt.cpp
@@ -102,7 +102,7 @@ struct RangeCutter{
     }
 
     if (O.get<int>("set_brd_from_range", 0) && cnv && !range.empty()){
-      newbrd=cnv->line_bck(rect2line(range));
+      newbrd=cnv->line_bck(rect2line(range), 1e-6);
     }
     cnt_o=0; cnt_l=0; cnt_p=0;
   }
