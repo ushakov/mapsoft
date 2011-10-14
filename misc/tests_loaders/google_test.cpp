0569d8758f5338ba0791870f5da1fb42c4c45e45
Vladislav Zavjalov <slazav@altlinux.org>
2011-10-15 02:59:35 +0400

    move google_test to misc/ dir

--- misc/tests_loaders/google_test.cpp
+++ misc/tests_loaders/google_test.cpp
@@ -7,36 +7,21 @@
 #include "loaders/image_google.h"
 
 int main(){
-  std::string dir="/e2/M/GOOGLE/";
+  std::string dir="/d3/GOOGLE/";
   int zoom=2;
   iRect src(0,0,512,512); // весь мир в масштабе 2
   // создаем картинку c картой мира
   iImage image = google::load(dir, zoom, src); 
 
-
   // в ее левый-верхний угол загружаем кусочек c заданными координатами wgs84:
-  zoom = 5;
-  dRect src_wgs84(90,50,20,10);
+  zoom = 3;
+  dRect src_wgs84(20,20,400,400);
   iRect dst(10,10,200,200); 
 
-
-  google::load_to_image(dir, zoom, src_wgs84, image, dst);
-
-
-  // случайный доступ:
-  zoom=5;
-  google::points pts(dir, zoom);
-
-  for (int j=0;j<200;j++){
-    for (int i=0;i<200;i++){
-      int c = pts.get(dPoint(90.0 + i/10.0, 50.0 + j/20.0));
-//      int c = pts.get(iPoint(i*2,j*2));
-      image.set(i+300,500-j,c);
-    }
-  }
+  google::load(dir, zoom, src_wgs84, image, dst);
 
   // сохраняем в jpeg
-  image_jpeg::save("google_test.jpg", image, 75);
+  image_jpeg::save(image, "google_test.jpg", 75);
 }
 
 
