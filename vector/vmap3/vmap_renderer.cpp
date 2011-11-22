7af896e6e561bf6c3ba5eacc5f6d181a399db4e8
Vladislav Zavjalov <slazav@altlinux.org>
2011-11-22 19:55:01 +0400

    Revert "vmap_render: small fix"
    This reverts commit dd85073f667de18786331e4109b6b834c5b91957.

--- vector/vmap3/vmap_renderer.cpp
+++ vector/vmap3/vmap_renderer.cpp
@@ -43,8 +43,9 @@ VMAPRenderer::VMAPRenderer(vmap::world * _W, int w, int h,
   cr->restore();
   cr->clip_preserve();   // set clipping region
   cr->save();
-  cr->set_color(0xFFFFFF);
-  cr->paint();
+  cr->set_source_rgb(1,1,1); // erase border inside clip region
+  cr->set_line_width(10);
+  cr->stroke();
   cr->restore();
 
   // modify vmap
