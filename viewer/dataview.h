b7f0af4d4f42753fa629e023a539f06eaab58d65
Vladislav Zavjalov <slazav@altlinux.org>
2012-01-09 22:16:21 +0400

    viewer: add Join button to the LayerList

--- viewer/dataview.h
+++ viewer/dataview.h
@@ -6,7 +6,7 @@
 class LayerListButtons : public Gtk::HBox {
 public:
   LayerListButtons();
-  Gtk::Button *up, *down, *save, *del, *jump;
+  Gtk::Button *up, *down, *save, *del, *jump, *join;
 };
 
 class Mapview;
@@ -15,6 +15,7 @@ class DataView : public Gtk::Notebook {
     Mapview * mapview;
     void layer_del();
     void layer_jump();
+    void layer_join();
     void layer_move(bool up);
     void layer_save();
     void on_layer_save(int r);
