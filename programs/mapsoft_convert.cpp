0330ec8763fcd39a701a180fdb5e1e4b6da60e5a
Vladislav Zavjalov <slazav@altlinux.org>
2012-02-12 18:42:05 +0400

    mapsoft_convert: use options/m_getopt.h instead of options/m_readconf.h

--- programs/mapsoft_convert.cpp
+++ programs/mapsoft_convert.cpp
@@ -11,24 +11,35 @@
 #include <math.h>
 
 #include "geo_io/io.h"
-#include "options/read_conf.h"
+#include "options/m_getopt.h"
 
 using namespace std;
 
-void usage(const char *fname){
+#define OPT1  1  //
+#define OPT2  2  // raster-options
 
-  cerr << "\nConvert geodata to different formats\n"
-       << "Usage: "<< fname << " <in1> ... <inN> [--option[=value]] ... -o <out>\n\n";
+static struct ext_option options[] = {
+  {"out",                   1,'o', OPT1, ""},
+  {"help",                  0,'h', OPT1, "show this message"},
+  {"verbose",               0,'v', OPT1, "be verbose\n"},
 
-  cerr << "Options:\n"
-       << "  --help, -h                    Show this help message\n"
-       << "  --out=<file>, -o <file>       Specify output file (required)\n"
-       << "  --skip=<string>, -s <string>  Skip some types of data\n"
-       << "  --rescale_maps <factor>       Rescale map references\n"
-       << "  --shift_maps <x>,<y>          Shift map references\n"
-       << "\n";
+  {"shift_maps",            1,  0, OPT1, "shift map references, \"x,y\""},
+  {"rescale_maps",          1,  0, OPT1, "rescale map references"},
+  {"map_nom_brd",           0,  0, OPT1, "set map borders according to map name"},
+  {"skip",                  1,'s', OPT1, "skip data, \"wmtao\" (w - waypoints, m - maps, t - tracks, a - active log, o - save tracks)"},
+  {"gen_n",                 1,  0, OPT1, "reduce track points to n"},
+  {"gen_e",                 1,  0, OPT1, "reduce track points up to accuracy e [meters] (when gen_n and gen_e both used it means: \"remove points while number of points > n OR accuracy < e\""},
 
-  cerr << "Input files (format is determined by file extension):\n"
+  {0,0,0,0}
+};
+
+void usage(){
+
+  const char * fname = "mapsoft_convert";
+  cerr << fname << " -- convert geodata between different formats\n"
+       << "Usage: "<< fname << " <options> <input files> -o <output file>\n"
+       << "\n"
+       << "Input files (format is determined by file extension):\n"
        << "  *.xml -- mapsoft native XML-like format\n"
        << "  *.fig -- mapsoft geofig format\n"
        << "  *.wpt, *.plt, *.map -- OziExplorer format\n"
@@ -37,9 +48,8 @@ void usage(const char *fname){
        << "  *.zip -- zipped files\n"
        << " gps: --  read data from Garmin GPS via autodetected serial device\n"
        << " <character device> -- read data from Garmin GPS via serial device\n"
-       << "\n";
-
-  cerr << "Output file (format is determined by file extension):\n"
+       << "\n"
+       << "Output file (format is determined by file extension):\n"
        << "Format is determined by file extension:\n"
        << "  *.xml -- mapsoft native XML-like format\n"
        << "  *.wpt, *.plt, *.map, *.oe  -- OziExplorer format\n"
@@ -53,40 +63,51 @@ void usage(const char *fname){
        << "  *.tif, .tiff, .jpg, .jpeg -- raster image (map, fig, or html wrap can be made)\n"
        << " gps: --  send data to Garmin GPS via autodetected serial device\n"
        << " <character device> -- send data to Garmin GPS via serial device\n"
-       << "\n";
-
-  cerr << "Skipping data. If --skip parameter string contains letter:\n"
-       << "  \"m\" -- skip maps\n"
-       << "  \"w\" -- skip waypoints\n"
-       << "  \"t\" -- skip tracks\n"
-       << "  \"a\" -- skip ACTIVE LOG track\n"
-       << "  \"o\" -- skip all tracks excluding ACTIVE LOG\n"
-       << "\n";
-
-  exit(0);
+       << "\n"
+       << "Options:"
+  ;
+  print_options(options, 3, cerr);
+  exit(1);
 }
 
 int main(int argc, char *argv[]) {
 try{
 
-  Options opts;
+  if (argc==1) usage();
 
-  if (!read_conf(argc, argv, opts)) usage(argv[0]);
-  if (opts.exists("help")) usage(argv[0]);
-
-  string outfile = opts.get("out", string());
-  if (outfile == "") usage(argv[0]);
-
-  StrVec infiles = opts.get("cmdline_args", StrVec());
-
-// чтение файлов
+  Options O = parse_options(&argc, &argv, options, 3);
+  if (O.exists("help")) usage();
 
   geo_data world;
-  for(StrVec::const_iterator i=infiles.begin(); i!=infiles.end(); i++)
-    io::in(*i, world, opts);
+  vector<string> infiles;
 
-  io::filter(world, opts);
-  io::out(outfile, world, opts);
+  while (argc>0) {
+    infiles.push_back(argv[0]);
+    Options O1 = parse_options(&argc, &argv, options, 3);
+    O.insert(O1.begin(), O1.end());
+  }
+
+  if (!O.exists("out")){
+    cerr << "no output files.\n";
+    exit(1);
+  }
+
+  if (O.exists("verbose")) cerr << "Reading data...\n";
+
+  for (vector<string>::const_iterator i = infiles.begin(); i!=infiles.end(); i++){
+    io::in(*i, world, O);
+  }
+
+  if (O.exists("verbose")){
+    cerr << "Map lists: " << world.maps.size()
+         << ",  Waypoint lists: " << world.wpts.size()
+         << ",  Tracks: " << world.trks.size() << "\n";
+  }
+
+  if (O.exists("verbose")) cerr << "Applying filters...\n";
+  io::filter(world, O);
+  if (O.exists("verbose")) cerr << "Writing data to " << O.get("out", string()) << "\n";
+  io::out(O.get("out", string()), world, O);
 
 } catch (const char *err){
   cerr << "ERROR: " << err << "\n";
