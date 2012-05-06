27bb53c655f374e036fdf264cd3e04af2ab6e9d4
Vladislav Zavjalov <slazav@altlinux.org>
2012-05-06 22:22:33 +0400

    mapsoft_toxyz: more flags

--- programs/mapsoft_toxyz.cpp
+++ programs/mapsoft_toxyz.cpp
@@ -30,12 +30,20 @@ void usage(bool pod=false){
        << "  %x -- wgs tmerc x\n"
        << "  %y -- wgs tmerc y\n"
        << "  %z -- altitude, m\n"
-       << "  %u -- time from the previous point, s\n"
+
        << "  %t -- time (seconds since 1970-01-01 00:00:00 UTC)\n"
        << "  %T -- date and time (yyyy-mm-dd HH:MM:SS)\n"
-       << "  %d -- % distance from the previous point, m\n"
-       << "  %D -- % distance from the beginning, km\n"
+       << "  %u -- time from the previous point, s\n"
+
+       << "  %d -- % distance from the part beginning, km\n"
+       << "  %D -- % distance from the data beginning, km\n"
+       << "  %e -- % distance from the previous point, m\n"
+
        << "  %S -- % speed, km/h\n"
+
+       << "  %n -- %  point number starting from current part\n"
+       << "  %N -- %  point number staarting from the beginning\n"
+       << "  %p -- %  part number\n"
   ;
   exit(1);
 }
@@ -64,10 +72,12 @@ int main (int argc, char **argv) {
   convs::pt2pt pc(Datum("wgs84"), Proj("tmerc"), geo_opts,
                     Datum("wgs84"), Proj("lonlat"), Options());
 
-  double speed=0, distance = 0;
+  double speed=0, dist = 0, Dist = 0;
   queue<pair<double, double> > timedist;
 
-  int k = 0;
+  int nn = 0;
+  int NN = 0;
+  int pn = 0;
   g_trackpoint tp, pp;
   for (int i = 0; i < world.trks.size(); ++i) {
     for (int p = 0; p < world.trks[i].size(); ++p) {
@@ -76,20 +86,22 @@ int main (int argc, char **argv) {
       pc.bck(tp); // convert lonlat -> tmerc
 
       if ( (br == "track" && (tp.start || p==0)) ||
-           (br == "day"   && (k != 0) &&
+           (br == "day"   && (nn != 0) &&
              (tp.t.value/86400 != pp.t.value/86400)) ){
-        k=0;
+        nn=0;
+        pn++;
         cout << endl;
       }
 
       double dd=0;
       int    dt=0;
-      if (k != 0) {
+      if (nn != 0) {
         // update values:
         dd = hypot(tp.x - pp.x, tp.y - pp.y);
         dt = tp.t.value - pp.t.value;
-        distance +=dd;
-        timedist.push(make_pair(tp.t.value, distance));
+        dist +=dd;
+        Dist +=dd;
+        timedist.push(make_pair(tp.t.value, Dist));
         while (timedist.front().first < tp.t.value - window &&
                timedist.size() > 2) timedist.pop();
 
@@ -97,11 +109,12 @@ int main (int argc, char **argv) {
         double time = timedist.back().first - timedist.front().first;
         speed = traveled/time * 3.6;
       } else {
-        distance = 0;
+        dist = 0;
         speed = 0;
       }
       pp = tp;
-      ++k;
+      ++nn;
+      ++NN;
 
       // print values according to fmt
       bool esc=false;
@@ -118,12 +131,20 @@ int main (int argc, char **argv) {
           case 'x': cout << (int)tp.x; break;
           case 'y': cout << (int)tp.y; break;
           case 'z': cout << fixed << setprecision(1) << tp.z; break;
-          case 'T': cout << tp.t; break;
+
           case 't': cout << tp.t.value; break;
+          case 'T': cout << tp.t; break;
           case 'u': cout << dt;         break;
-          case 'd': cout << fixed << setprecision(2) << dd; break;
-          case 'D': cout << fixed << setprecision(3) << distance/1000.0; break;
+ 
+          case 'd': cout << fixed << setprecision(2) << dist/1000.0; break;
+          case 'D': cout << fixed << setprecision(3) << Dist/1000.0; break;
+          case 'e': cout << fixed << setprecision(2) << dd; break;
+
           case 'S': cout << fixed << setprecision(2) << speed;    break;
+
+          case 'n': cout << nn+1;    break;
+          case 'N': cout << NN+1;    break;
+          case 'p': cout << pn+1;    break;
           default:
             cout << '%' << *c;
         }
