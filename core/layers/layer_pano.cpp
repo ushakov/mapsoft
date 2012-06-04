f1837d7ff9b9a2311f24e219cbb114246fdf562f
Vladislav Zavjalov <slazav@altlinux.org>
2012-06-04 13:17:07 +0400

    layer_pano: cleanup, comments

--- core/layers/layer_pano.cpp
+++ core/layers/layer_pano.cpp
@@ -8,7 +8,6 @@ LayerPano::LayerPano(srtm3 * s): srtm(s), ray_cache(512) {
   opt.put<double>("pano_minh", 0.0);
   opt.put<double>("pano_maxh", 6000.0);
   opt.put<double>("pano_alt",  20.0);
-  opt.put<double>("pano_minr", 100.0);
   opt.put<double>("pano_maxr", 60000.0);
   opt.put<dPoint>("pano_pt",   dPoint());
 }
@@ -27,8 +26,11 @@ LayerPano::set_scale(const double k){
 
 void
 LayerPano::set_opt(const Options & o){
+  // TODO -- separate setting for p0, h, colors...
+  // We need to clear ray_cache only if
+  //  pano_pt or pano_maxr changes
   opt = o;
-  ray_cache.clear();  // todo -- separate setting for p0, h...
+  ray_cache.clear();
 }
 
 Options
@@ -46,15 +48,15 @@ LayerPano::get_ray(dPoint pt, int x, double max_r){
   //a is counted clockwise from north in radians
   double a = 2*M_PI*x/w;
   int key=round(a*1000000);
-
   if (ray_cache.contains(key)) return ray_cache.get(key);
-
 
   double sa=sin(a), ca=cos(a), cx=cos(pt.y * M_PI/180.0);
   pt*=1200; // srtm units
   max_r*=srtm_width * 180/M_PI/ 6380000;
 
-  double rx,drx,ry,dry;
+  // Intersections or ray with x and y lines of srtm grid goes
+  // with constant steps. But we need to sort them by r.
+  double rx,drx,ry,dry; // Initial values and steps
   if      (sa>1/max_r) { drx=cx/sa;  rx=(ceil(pt.x)-pt.x)*cx/sa;  }
   else if (sa<-1/max_r){ drx=-cx/sa; rx=(floor(pt.x)-pt.x)*cx/sa; }
   else                 { drx=rx=max_r; }
@@ -63,14 +65,14 @@ LayerPano::get_ray(dPoint pt, int x, double max_r){
   else                 { dry=ry=max_r; }
 
   vector<LayerPano::ray_data> ret;
-//  double max_h=-1000;
-  while (rx<max_r || ry<max_r){
-    while (rx <= ry){
-      int x = round(pt.x+rx*sa/cx);
-      double y = pt.y+rx*ca;
+  while (rx<max_r || ry<max_r){ // Go from zero to max_r
 
-      // assert(abs(pt.x+rx*sa/cx - x) < 1e-9);
+    while (rx <= ry){ // step in x
+      int x = round(pt.x+rx*sa/cx); // x is a round number
+      double y = pt.y+rx*ca;        // y is between two grid points.
+      // assert(abs(pt.x+rx*sa/cx - x) < 1e-9); // check this
 
+      // Interpolate altitude and slope between two points:
       int y1 = floor(y);
       int y2 = ceil(y);
       double h,s;
@@ -86,14 +88,13 @@ LayerPano::get_ray(dPoint pt, int x, double max_r){
         h = srtm->geth(x,y1);
         s = srtm->slope(x,y1);
       }
+      // Save data and do step:
       ret.push_back(ray_data(rx, h, s));
-//      if (max_h<h) max_h=h;
       rx+=drx;
     }
-    while (ry <= rx){
+    while (ry <= rx){ // step in y; the same but rx->ry, y is on the grid
       double x = pt.x+ry*sa/cx;
       int y = round(pt.y+ry*ca);
-
       // assert(abs(pt.y+ry*ca - y) < 1e-9);
 
       int x1 = floor(x);
@@ -112,11 +113,9 @@ LayerPano::get_ray(dPoint pt, int x, double max_r){
         s = srtm->slope(x1,y);
       }
       ret.push_back(ray_data(ry, h, s));
-//      if (max_h<h) max_h=h;
       ry+=dry;
     }
   }
-//  if (ret.size()) ret[0].h=max_h;
   ray_cache.add(key,ret);
   return ret;
 }
@@ -135,39 +134,35 @@ LayerPano::draw(iImage & image, const iPoint & origin){
   dPoint p0    = opt.get<dPoint>("pano_pt");
   simple_rainbow rb(min_h, max_h);
 
-//  const double RE = 6380000.0;
-//  const double d2r = M_PI/180.0;
-//  double cc = cos(p0.y * d2r);
-  double h0 = (double)srtm->geth4(p0) + dh;
-
-//  int mode=0; // whole view is above terrain
-
+  double h0 = (double)srtm->geth4(p0) + dh; // altitude of observation point
   for (int x=0; x < image.w; x++){
-
-//std::cerr << "XX " << x << "\n";
-    // get ray data
+    // get ray data -- r,h,s values for a giver x-coord
     vector<ray_data> ray = get_ray(p0,x+origin.x,max_r);
     if (!ray.size()) continue;
-//    int hM = ray[0].h; // max altitude
 
-    int yo = image.h;
-    int yp = w/4.0-origin.y;
-    double hp, sp;
+    int yo = image.h;        // Old y-coord, previously painted point.
+                             // It is used to skip hidden parts.
+    int yp = w/4.0-origin.y; // Previous value, differs from yo on hidden 
+                             // and partially hydden segments.
+                             // It is used to interpolate height and slope.
+                             // Y axis goes from top to buttom!
     for (int i=1; i<ray.size(); i++){
-      double hp=ray[i-1].h;
+      double hp=ray[i-1].h;  // altitudes and slopes at the end of segment
       double sp=ray[i-1].s;
       double hn=ray[i].h;
       double sn=ray[i].s;
-      double r=ray[i].r /=1200 * 180/M_PI/ 6380000;
-      double b = atan((hn-h0)/r);
+      double r=ray[i].r /=1200 * 180/M_PI/ 6380000; // convert distance srtm units -> m
 
-      int yn = (1/2.0 - 2*b/M_PI) * w/4.0 - origin.y;
-      if (yn<0)  {i=ray.size();}
-      if (yn>=yo){yp=yn; continue; }
+      double b = atan((hn-h0)/r); // vertical angle
+      int yn = (1/2.0 - 2*b/M_PI) * w/4.0 - origin.y; // y-coord
+
+      if (yn<0)  {i=ray.size();}     // above image -- draw the rest of segment and exit
+      if (yn>=yo) {yp=yn; continue;} // point below image -- skip segment
+
       for (int y = yn; y < yp; y++){
-        if (y<0 || y>=yo) continue;
-        double s = sp + (sn-sp)*(y-yp)/double(yn-yp);
-        double h = hp + (hn-hp)*(y-yp)/double(yn-yp);
+        if (y<0 || y>=yo) continue; // select visible points
+        double s = sp + (sn-sp)*(y-yp)/double(yn-yp); // Interpolate slope and altitude
+        double h = hp + (hn-hp)*(y-yp)/double(yn-yp); //  and calculate point color.
         int color = color_shade(rb.get(h), (1-r/max_r)*(1-s/90));
         image.set_na(x,y, color);
       }
@@ -175,7 +170,7 @@ LayerPano::draw(iImage & image, const iPoint & origin){
       yp=yn;
     }
 
-    for (int y = 0; y < yo; y++)
+    for (int y = 0; y < yo; y++) // dray sky points
       image.set_na(x,y,0xFFBBBB);
   }
   return GOBJ_FILL_ALL;
