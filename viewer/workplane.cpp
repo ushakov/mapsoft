4a24bfa19def5ebb1d2229ff138c6dead301e68b
Vladislav Zavjalov <slazav@altlinux.org>
2012-06-07 03:50:37 +0400

    viewer: fix workplane refreshing

--- viewer/workplane.cpp
+++ viewer/workplane.cpp
@@ -120,10 +120,8 @@ Workplane::get_layer_depth (LayerGeo * layer){
 void
 Workplane::refresh_layer (LayerGeo * layer, bool redraw){
 	layer->refresh();
-        if (redraw){
-           tile_cache[layer]->clear();
-           signal_refresh.emit();
-        }
+        tile_cache[layer]->clear();
+        if (redraw) signal_refresh.emit();
 }
 
 void
