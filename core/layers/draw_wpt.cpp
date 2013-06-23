63df1d74afdcdd3366d59eaea68a4ebe28ba587b
Vladislav Zavjalov <slazav@altlinux.org>
2013-06-23 11:33:03 +0400

    layers: split draw_wpt into make_wpts_tmpl and plot_wpts_tmpl (will be used for caching)

--- core/layers/draw_wpt.cpp
+++ core/layers/draw_wpt.cpp
@@ -1,59 +1,91 @@
 #include <vector>
 #include <cmath>
 
-#include "layer_wpt.h"
-#include "utils/cairo_wrapper.h"
+#include "draw_wpt.h"
 
 using namespace std;
+
+tmpl_wpts
+make_wpts_tmpl(iImage & image, const iPoint & origin,
+                const Conv & cnv, const g_waypoint_list & wpt,
+                const Options & opt, dRect & range){
+  tmpl_wpts ret;
+  g_waypoint_list::const_iterator pt;
+  CairoWrapper cr(image);
+  range=dRect();
+
+  for (pt=wpt.begin(); pt!=wpt.end(); pt++){
+    tmpl_wpt p;
+    p.x=pt->x; p.y=pt->y; cnv.bck(p); p-=origin;
+    p.color = pt->color.value;
+    p.bgcolor = pt->bgcolor.value;
+    p.name=pt->name;
+
+    // dimensions
+    p.font_size  = pt->font_size * 1.5;
+    p.dot_rad    = pt->size      * 0.25;
+    p.text_pad   = p.font_size * 0.2;
+    double flag_shift = p.font_size * 0.5;
+
+    dPoint text_shift(p.text_pad, -p.dot_rad - flag_shift - p.text_pad);
+
+    // find text bbox
+    cr->set_font_size(p.font_size);
+    cr->move_to(p + text_shift);
+    p.txt_rect = cr->get_text_extents(p.name);
+    p.txt_rect = rect_pump(p.txt_rect, p.text_pad);
+
+    // find point bbox
+    dRect pt_rect(p - dPoint(p.dot_rad, p.dot_rad+flag_shift+p.txt_rect.h),
+                  p + dPoint(min(p.dot_rad,p.txt_rect.w), p.dot_rad));
+    pt_rect = rect_pump(pt_rect, 1.0); // linewidth
+
+    // update range
+    if (range.empty()) range=pt_rect;
+    else range = rect_bounding_box(range, pt_rect);
+
+    // add point to the cache if needed
+    if (!rect_intersect(pt_rect, dRect(image.range())).empty())
+      ret.push_back(p);
+  }
+  return ret;
+}
+
+void
+plot_wpts_tmpl(const tmpl_wpts & wpts, CairoWrapper & cr){
+  std::vector<tmpl_wpt>::const_iterator i;
+  for (i=wpts.begin(); i!=wpts.end(); i++){
+    cr->move_to(*i);
+    cr->circle(*i, i->dot_rad);
+    cr->rectangle(i->txt_rect);
+
+    cr->set_color(i->bgcolor);
+    cr->fill_preserve();
+
+    cr->set_line_width(1);
+    cr->set_color(i->color);
+    cr->stroke();
+
+    cr->move_to(*i + dPoint(0,-i->dot_rad));
+    cr->rel_line_to(i->txt_rect.BLC());
+    cr->stroke();
+
+    cr->move_to(i->txt_rect.BLC());
+    cr->rel_move_to(dPoint(i->text_pad, -i->text_pad));
+    cr->set_font_size(i->font_size);
+    cr->show_text(i->name);
+  }
+}
 
 void
 draw_wpt(iImage & image, const iPoint & origin,
          const Conv & cnv, const g_waypoint_list & wpt,
          const Options & opt){
 
+  dRect range;
+  tmpl_wpts cache =
+    make_wpts_tmpl(image, origin, cnv, wpt, opt, range);
+
   CairoWrapper cr(image);
-
-  g_waypoint_list::const_iterator pt;
-  for (pt=wpt.begin(); pt!=wpt.end(); pt++){
-    dPoint p(pt->x,pt->y); cnv.bck(p); p-=origin;
-
-    Color color = pt->color;
-    Color bgcolor = pt->bgcolor;
-
-    double fs = pt->font_size * 1.5;
-    double dot_width = pt->size/4.0;
-    dPoint flag_shift(0,-fs);
-    double text_pad=fs * 0.2;
-
-    dPoint text_shift = flag_shift + dPoint(text_pad, -text_pad);
-    cr->set_font_size(fs);
-
-    cr->move_to(p + text_shift);
-    dRect txt_rect = cr->get_text_extents(pt->name);
-    txt_rect = rect_pump(txt_rect, text_pad);
-    txt_rect = rect_pump(txt_rect, p + flag_shift);
-    dRect pt_rect = rect_pump(txt_rect, p);
-    pt_rect = rect_pump(pt_rect, dot_width+1);
-
-    if (!rect_intersect(pt_rect, dRect(image.range())).empty()){
-      cr->move_to(p);
-      cr->circle(p, dot_width);
-      cr->rectangle(txt_rect);
-      cr->set_color(bgcolor.value);
-      cr->fill();
-
-      cr->move_to(p);
-      cr->circle(p, dot_width);
-      cr->move_to(p+dPoint(0,-dot_width));
-      cr->rel_line_to(flag_shift);
-      cr->rectangle(txt_rect);
-      cr->set_color(color.value);
-      cr->set_line_width(1);
-      cr->stroke();
-
-      cr->set_color(color.value);
-      cr->move_to(p + text_shift);
-      cr->show_text(pt->name);
-    }
-  }
+  plot_wpts_tmpl(cache, cr);
 }
--- core/layers/draw_wpt.h
+++ core/layers/draw_wpt.h
@@ -5,10 +5,36 @@
 #include "2d/conv.h"
 #include "geo/g_wpt.h"
 #include "options/options.h"
+#include "utils/cairo_wrapper.h"
 
-void
-draw_wpt(iImage & image, const iPoint & origin,
-         const Conv & cnv, const g_waypoint_list & wpt,
-         const Options & opt);
+/* draw waypoints */
+void draw_wpt(iImage & image, const iPoint & origin,
+              const Conv & cnv, const g_waypoint_list & wpt,
+              const Options & opt);
+
+/***********************************************************/
+
+/* Drawing can be splitted into two parts:
+   Creating small drawing template (can be slow)
+   and then drawing it.
+   Also, while creating template we go through
+   all wpts and can calculate the total range */
+
+/* waypoint templates */
+struct tmpl_wpt : dPoint{
+  double font_size, dot_rad, text_pad;
+  int color, bgcolor;
+  dRect txt_rect;
+  std::string name;
+};
+typedef std::vector<tmpl_wpt> tmpl_wpts;
+
+/* make template and calculate total range */
+tmpl_wpts make_wpts_tmpl(iImage & image, const iPoint & origin,
+                             const Conv & cnv, const g_waypoint_list & wpt,
+                             const Options & opt, dRect & range);
+
+/* plot template */
+void plot_wpts_tmpl(const tmpl_wpts & wpts, CairoWrapper & cr);
 
 #endif
