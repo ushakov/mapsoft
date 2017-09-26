1711ffda967f7d5e418270235de86143b90571c6
Vladislav Zavjalov <slazav@altlinux.org>
2017-09-27 01:07:28 +0300

    core/getopt: separate general options in the test

--- core/getopt/getopt.test.cpp
+++ core/getopt/getopt.test.cpp
@@ -4,22 +4,24 @@
 #include "getopt.h"
 #include <cstring>
 
-#define OPT_INP  1  // input-only options
-#define OPT_CMN  2  // filter options (used both as input and output)
-#define OPT_OUT  4  // output-only options
-#define OPT_STP  8  // special option -o/--out
+#define OPT_IN0  1  // global input options (-v, -h)
+#define OPT_INP  2  // input-only options
+#define OPT_CMN  4  // filter options (used both as input and output)
+#define OPT_OUT  8  // output-only options
+#define OPT_STP 16  // special option -o/--out
 
 #define MASK_INP  (OPT_INP | OPT_CMN | OPT_STP) // mask to select input options
-#define MASK_OUT  (OPT_OUT | OPT_CMN) // mask to select output options
+#define MASK_IN0  (MASK_INP | OPT_IN0) // mask to select global input options
+#define MASK_OUT  (OPT_OUT | OPT_CMN)  // mask to select output options
 
 using namespace std;
 
 static struct ext_option options[] = {
   {"out",                   0,'o', OPT_STP, ""},
 
-  {"help",                  0,'h', OPT_INP, "show help message"},
-  {"pod",                   0, 0 , OPT_INP, "show this message as POD template"},
-  {"verbose",               0,'v', OPT_INP, "be verbose\n"},
+  {"help",                  0,'h', OPT_IN0, "show help message"},
+  {"pod",                   0, 0 , OPT_IN0, "show this message as POD template"},
+  {"verbose",               0,'v', OPT_IN0, "be verbose\n"},
 
   {"inp1",                  1,'I', OPT_INP, "input option with argument"},
   {"inp2",                  0,'J', OPT_INP, "input option w/o argument"},
@@ -35,7 +37,7 @@ void usage(bool pod=false){
   const char * prog = "getopt.test";
   cerr << prog << " -- example of mapsoft-style getopt\n"
      << head << "Usage:\n"
-     << prog << " [<global_input_options>]\\\n"
+     << prog << " [<general_options>|<global_input_options>]\\\n"
      << "         <input_file_1> [<input_options_1>]     \\\n"
      << "         <input_file_2> [<input_options_2>] ... \\\n"
      << "         (--out|-o) <output_file> [<output_options>]\n"
@@ -45,11 +47,13 @@ void usage(bool pod=false){
      << "Then all data should be processed and written to the\n"
      << "output file according with output options.\n"
   ;
-  cerr << head << "Common Options (can be used as input and output options):\n";
-  print_options(options, OPT_CMN, cerr, pod);
-  cerr << head << "Input Options:\n";
+  cerr << head << "General options:\n";
+  print_options(options, OPT_IN0, cerr, pod);
+  cerr << head << "Input options:\n";
   print_options(options, OPT_INP, cerr, pod);
-  cerr << head << "Output Options:\n";
+  cerr << head << "Common options (can be used as input and output options):\n";
+  print_options(options, OPT_CMN, cerr, pod);
+  cerr << head << "Output options:\n";
   print_options(options, OPT_OUT, cerr, pod);
   throw Err();
 }
@@ -61,7 +65,7 @@ main(int argc, char *argv[]){
 
     if (argc<2) usage();
 
-    Opts O = parse_options(&argc, &argv, options, MASK_INP, "out");
+    Opts O = parse_options(&argc, &argv, options, MASK_IN0, "out");
     if (O.exists("help")) usage();
     if (O.exists("pod"))  usage(true);
     bool verb = O.exists("verbose");
