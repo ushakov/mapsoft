674cf6c8f235324beb4f69d806210655bf39fd9a
Vladislav Zavjalov <slazav@altlinux.org>
2019-11-16 21:45:43 +0200

    core/tiles/convs_gtiles: add -bi, -bc options, thanks to ioctl@ya

--- core/tiles/convs_gtiles.cpp
+++ core/tiles/convs_gtiles.cpp
@@ -1,7 +1,10 @@
 #include "tiles.h"
 #include "opts/opts.h"
+#include "geo_io/geo_refs.h"
 #include <iostream>
 #include <cstring>
+#include <vector>
+#include <boost/geometry.hpp>
 
 using namespace std;
 
@@ -11,6 +14,8 @@ void usage(){
        << "usage: convs_tile -p <point> <z> -- tile which covers a WGS84 point\n"
        << "       convs_tile -r <range> <z> -- tiles which cover a range (return a rectangle)\n"
        << "       convs_tile -R <range> <z> -- tiles which cover a range (return a list)\n"
+       << "       convs_tile -bi <track> <z> -- tiles which touches the area (return a list)\n"
+       << "       convs_tile -bc <track> <z> -- tiles which fully covered by area (return a list)\n"
        << "       convs_tile -n x y z -- tile range\n"
        << "       convs_tile -c x y z -- tile center\n"
        << "       convs_tile -t x y z <range>  -- check if a tile touches a range\n"
@@ -45,6 +50,80 @@ int main(int argc, char** argv){
     for (int y = ret.y; y<ret.y+ret.h; y++)
       for (int x = ret.x; x<ret.x+ret.w; x++)
         cout << x << "," << y << "," << z << "\n";
+    return 0;
+  }
+
+  if ((argc == 4) && ((strcmp(argv[1], "-bi") == 0)
+      || (strcmp(argv[1], "-bc") == 0))){
+    using namespace boost::geometry;
+    int zfin = str_to_type<int>(argv[3]);
+    geo_data B;
+    vector<g_track>::const_iterator bi;
+    vector<g_trackpoint>::const_iterator pi;
+    typedef model::d2::point_xy<double> point_t;
+    std::vector<point_t> pts;
+    typedef model::polygon<point_t> polygon_t;
+    polygon_t poly;
+    typedef struct {int x; int y; int z;} tile_s;
+    vector<tile_s> oldtiles, newtiles = {{0, 0, 0}};
+    vector<tile_s>::const_iterator ti;
+
+    try{
+      io::in(string(argv[2]), B);
+    }
+    catch(Err e) {
+      cerr << e.get_error() << endl;
+      return 1;
+    }
+
+    // Прочитаем трек в полигон
+    pts.clear();
+    bi=B.trks.begin();
+    for (pi=bi->begin(); pi!=bi->end(); pi++){
+      pts.push_back(point_t(pi->x, pi->y));
+    }
+    pts.push_back(point_t(bi->begin()->x, bi->begin()->y));
+    append(poly, pts);
+
+    /* Переберём все возможные тайлы
+       методом последовательных приближений */
+    for (int z = 1; z <= zfin; z++) {
+      oldtiles=newtiles;
+      newtiles.clear();
+      // Переберём все тайлы с прошлого приближения
+      for (ti=oldtiles.begin(); ti!=oldtiles.end(); ti++) {
+        // Поделим подходящий тайл на 4 новых и проверим каждый из них
+        for (int i = 0; i < 4; i++) {
+          int flag;
+          int x = ti->x * 2 +   (i & 1);
+          int y = ti->y * 2 + !!(i & 2);
+          polygon_t tile;
+          dRect tr = calc.gtile_to_range(x, y, z);
+          pts.clear();
+          pts.push_back(point_t(tr.x,        tr.y       ));
+          pts.push_back(point_t(tr.x + tr.w, tr.y       ));
+          pts.push_back(point_t(tr.x + tr.w, tr.y + tr.h));
+          pts.push_back(point_t(tr.x       , tr.y + tr.h));
+          pts.push_back(point_t(tr.x,        tr.y       ));
+          append(tile, pts);
+
+          if ((z == zfin) && strcmp(argv[1], "-bi") != 0) {
+            /* Полное покрытие */
+            flag = covered_by(tile, poly);
+          } else {
+            /* Простое пересечение */
+            flag = intersects(tile, poly);
+          }
+          if (flag) {
+            tile_s tt = {x, y, z};
+            newtiles.push_back(tt);
+          }
+        }
+      }
+    }
+    for (ti=newtiles.begin(); ti!=newtiles.end(); ti++) {
+      cout << ti->x << " " << ti->y << " " << ti->z << endl;
+    }
     return 0;
   }
 
